f2f4d21d5db59fb0b8db099d72b7a8788547db5d
vipul-rawat-zs <vipul.rawat@zopsmart.com>
2024-04-02 17:32:06 +0530

    add skeleton functionality

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
-module vipul-rawat/gofr-httpDB
+module github.com/vipul-rawat/gofr-httpDB
 
 go 1.22
+
+replace (
+	gofr.dev v1.2.1 => /Users/raramuri/go/src/github.com/gofr-dev/gofr
+)
 
 require gofr.dev v1.2.1 // indirect
---
+++ main.go
@@ -0,0 +1,23 @@
+package httpDB
+
+import "gofr.dev/pkg/gofr/datasource"
+
+type httpDB struct {
+	logger  Logger
+	metrics Metrics
+}
+
+var DB = func(conf datasource.Config, logger datasource.Logger, metrics datasource.Metrics) *httpDB {
+	return &httpDB{
+		logger:  logger,
+		metrics: metrics,
+	}
+}
+
+func (h *httpDB) Get(key string) string {
+	return ""
+}
+
+func (h *httpDB) Set(key, value string) {
+	return
+}
